184db333bf32c3b86d8c69277e84d3ac31f77b16
Guilhem Torgue <torgue.guilhem@gmail.com>
2019-03-26 14:29:11 +0100

    fonction round implementation

--- src/pokemon/Battle.ts
+++ src/pokemon/Battle.ts
@@ -1,13 +1,13 @@
 export class Battle {
-  public round: number
-  public bluePokemon: Pokemon
-  public redPokemon: Pokemon
-  public winner: Pokemon
+  public round: number;
+  public bluePokemon: Pokemon;
+  public redPokemon: Pokemon;
+  public winner: Pokemon;
 
   constructor(redPokemon: Pokemon, bluePokemon: Pokemon) {
-      this.round = 0
-      this.redPokemon = redPokemon
-      this.bluePokemon = bluePokemon
+      this.round = 0;
+      this.redPokemon = redPokemon;
+      this.bluePokemon = bluePokemon;
   }
 
   function priority(firstPokemon: Pokemon, secondPokemon: Pokemon) {
@@ -15,6 +15,14 @@ export class Battle {
   }
 
   function round() {
-    
+    if(priority(redPokemon, bluePokemon) === redPokemon) {
+      redPokemon.throwAtack(bluePokemon);
+      bluePokemon.throwAtack(redPokemon);
+    }
+    else {
+      bluePokemon.throwAtack(redPokemon);
+      redPokemon.throwAtack(bluePokemon);
+    }
+    this.round++;
   }
 }
