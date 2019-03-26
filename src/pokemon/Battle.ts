f2d49514d6b15d2d10533cbc42ab7f3f1d875f40
Guilhem Torgue <torgue.guilhem@gmail.com>
2019-03-26 11:34:42 +0100

    creation classe Battle

--- src/pokemon/Battle.ts
+++ src/pokemon/Battle.ts
@@ -1,3 +1,16 @@
 export class Battle {
-  constructor() {}
+  public round: number
+  public bluePokemon: Pokemon
+  public redPokemon: Pokemon
+  public winner: Pokemon
+
+  constructor(redPokemon: Pokemon, bluePokemon: Pokemon) {
+      this.round = 0
+      this.redPokemon = redPokemon
+      this.bluePokemon = bluePokemon
+  }
+
+  function priority(firstPokemon: Pokemon, secondPokemon: Pokemon) {
+    return firstPokemon.speed > secondPokemon ? firstPokemon : secondPokemon;
+  }
 }
