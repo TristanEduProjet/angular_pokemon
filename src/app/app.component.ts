d5265530bd8be668c1ff141097335228a13b1c49
Guilhem Torgue <torgue.guilhem@gmail.com>
2019-03-27 15:56:42 +0100

    Les attaques des familles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {Pokemon} from '../pokemon/Pokemon'
+/*import {Pokemon} from '../pokemon/Pokemon'
 import {Attack} from '../pokemon/Move'
 import {Battle} from '../pokemon/Battle'
-import {Type} from '../pokemon/Pokemon'
+import {Type} from '../pokemon/Pokemon'*/
 
 @Component({
   selector: 'app-root',
@@ -13,12 +13,12 @@ export class AppComponent {
   title = 'Pokemon Battle';
 
   constructor() {
-    let fireBall = new Attack('Boule de feu', 10, Type.ELECTRIQUE, 30);
+    /*let fireBall = new Attack('Boule de feu', 10, Type.ELECTRIQUE, 30);
     let eclair = new Attack('Petit eclair', 20, Type.ELECTRIQUE, 15);
     let dracofeu = new Pokemon('Dragon', 100, 30, fireBall, Type.ELECTRIQUE);
     let pika = new Pokemon('pikapika', 70, 50, eclair, Type.ELECTRIQUE);
 
     let battle = new Battle(dracofeu, pika);
-    battle.fight();
+    battle.fight();*/
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BattleComponent } from './battle/battle.component';
-import { BattlePokemonComponent } from './battle/battle-pokemon.component';
+//import { BattleComponent } from './battle/battle.component';
+//import { BattlePokemonComponent } from './battle/battle-pokemon.component';
 import { PrepareBattleComponent } from './prepare-battle/prepare-battle.component';
 import { SelectPokemonComponent } from './prepare-battle/select-pokemon.component';
-import { PokemonSimpleInfoComponent } from './prepare-battle/pokemon-simple-info.component';
+//import { PokemonSimpleInfoComponent } from './prepare-battle/pokemon-simple-info.component';
 
 @NgModule({
   declarations: [
