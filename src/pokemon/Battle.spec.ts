d57a22dca587459bf70ceeb68d85e45805f37cb2
Vash94350 <michael.pean.mp@gmail.com>
2019-03-28 15:12:08 +0100

    ajout de test pour les coups critiques et les defences

--- src/pokemon/Battle.spec.ts
+++ src/pokemon/Battle.spec.ts
@@ -81,5 +81,73 @@ describe('BattleTest', () => {
 
         expect(fireBall.isSuccessful(20)).toBeTruthy();
     });
+});
 
+describe('a hit can be transformed in critical hit', () => {
+  test('a hit is transformed in critical if his random value is equals to 0.04', () => {
+    const ferocerie = new Attack('Ferocerie', 40, 'electric', 100);
+    const pokemon_hiten = new Pokemon('pikachu', 40, 50, [eclair], 'electric');
+
+    expect(ferocerie.isCritical(0.04)).toBeTruthy();
+  });
+
+  test('a hit is transformed in critical if his random value is equals to 0.04', () => {
+    const ferocerie = new Attack('Ferocerie', 40, 'electric', 100);
+    const pokemon_hiten = new Pokemon('pikachu', 40, 50, [eclair], 'electric');
+
+    expect(ferocerie.isCritical(0.8)).toBeFalsy();
+  });
+
+    test('a critical hit is bigger than normal hit', () => {
+      const fireBall = new Attack('Boule de feu', 20, 'electric', 30);
+      const pokemon_hiten = new Pokemon('pikapika', 70, 50, [eclair], 'electric');
+
+
+      var degat;
+      expect(degat=fireBall.enhenceDammage()).toBeTruthy(fireBall.dommage<degat);
+    });
+
+    test('a critical hit is bigger than normal hit', () => {
+      const ferocerie = new Attack('Ferocerie', 40, 'electric', 100);
+      const pokemon_hiten = new Pokemon('pikachu', 40, 50, [eclair], 'electric');
+
+
+      var degat;
+      expect(degat=fireBall.enhenceDammage()).toBeTruthy(ferocerie.dommage<degat);
+    });
+});
+
+describe('defence', () => {
+  //Ajouter de la defence a chaque pokemon.
+    test('a pokemon (pika) who was hiten who has a def(15) must reduce the dommage(30)', () => {
+        const fireBall = new Attack('Boule de feu', 20, 'electric', 30);
+        const pokemon_hiten = new Pokemon('pikapika', 70, 50, [eclair], 'electric');
+
+        pokemon_hiten.reduceDammage(fireBall.dommage);
+        expect(pokemon_hiten.health_point).toBe(55);
+    });
+
+    test('a pokemon (pika) who was hiten who has a def(45) must cancel the dommage(30)', () => {
+        const fireBall = new Attack('Boule de feu', 20, 'electric', 30);
+        const pokemon_hiten = new Pokemon('pikapika', 70, 50, [eclair], 'electric');
+
+        pokemon_hiten.reduceDammage(fireBall.dommage));
+        expect(pokemon_hiten.health_point).toBe(70);
+    });
+
+    test('a pokemon (pika) who was hiten by low dommage(30) who has a def(45) not health', () => {
+        const fireBall = new Attack('Boule de feu', 20, 'electric', 30);
+        const pokemon_hiten = new Pokemon('pikapika', 70, 50, [eclair], 'electric');
+
+        pokemon_hiten.reduceDammage(fireBall.dommage));
+        expect(pokemon_hiten.health_point).toBe(70);
+    });
+
+    test('a pokemon (pika) who was hiten by low dommage(10) who has a def(10) not health', () => {
+        const fireBall = new Attack('Boule de feu', 20, 'electric', 10);
+        const pokemon_hiten = new Pokemon('pikapika', 70, 50, [eclair], 'electric');
+
+        pokemon_hiten.reduceDammage(fireBall.dommage));
+        expect(pokemon_hiten.health_point).toBe(70);
+    });
 });
