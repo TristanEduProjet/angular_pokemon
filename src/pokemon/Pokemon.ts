59d7babae023896f72b13c3bd6868dab4c57abcb
Vash94350 <michael.pean.mp@gmail.com>
2019-03-28 10:43:29 +0100

    methode de log pour l'envoi d'attaque

--- src/pokemon/Pokemon.ts
+++ src/pokemon/Pokemon.ts
@@ -12,13 +12,17 @@ export class Pokemon {
       setTimeout(() => {
         let randInt = Math.floor(Math.random() * this.abilities.length);
         pokemon_targeted.health_point-=this.abilities[randInt].dommage;
-        console.log(this.name + ' attaque ' + pokemon_targeted.name + ' avec ' + this.abilities[randInt].name);
-        console.log('Il reste ' + pokemon_targeted.health_point + ' a ' + pokemon_targeted.name);
+        this.log(this.name + ' attaque ' + pokemon_targeted.name + ' avec ' + this.abilities[randInt].name);
+        this.log('Il reste ' + pokemon_targeted.health_point + ' a ' + pokemon_targeted.name);
         resolve();
       }, 1500);
       return null;
     };
     return new Promise(prom);
+  }
+
+  protected log(msg: string) {
+      console.log(msg);
   }
 }
 
